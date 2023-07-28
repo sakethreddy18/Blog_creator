1e6a7b2f0cb37cff44e3e52f395504cfab84d634
sakethreddy18 <81903264+sakethreddy18@users.noreply.github.com>
2023-07-28 18:15:14 +0530

    Update BlogDetailsComponent.js

--- src/BlogDetailsComponent.js
+++ src/BlogDetailsComponent.js
@@ -4,11 +4,11 @@ import useFetch from "./useFetch"
 
 const BlogDetails = () => {
 const {id } = useParams()
-const {data:blog , error , isPending} = useFetch('http://localhost:8000/blogs/' + id)
+const {data:blog , error , isPending} = useFetch('https://sakethreddy18.github.io/jsonapi/Data/db.json/' + id)
 const nav = useNavigate()
 
 const handleClick = () => {
-    fetch('http://localhost:8000/blogs/'+ blog.id , {
+    fetch('https://sakethreddy18.github.io/jsonapi/Data/db.json/'+ blog.id , {
         method: 'DELETE'
     }).then(() => {
         nav("/")
